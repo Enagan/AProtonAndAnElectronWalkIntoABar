1bc2e1e99814b292a0fba2056f993e5f59af9c58
Enagan <pedro.a.f.engana@gmail.com>
2013-07-18 18:53:01 +0100

    Bipolar Console
    This is the first working implementation of our developer console. From
    here on out, BipolarDevID class should NOT be versioned, as soon as
    everyone gets his.
    This class's variables should be set to your personal preference.

---
+++ BipolarAlpha/Assets/Scripts/Services/DebugLogger/BipolarConsole.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using System.Collections;
+
+public enum Devs { ENGANA, LOUSADA, HUGO, IVO, RUI }
+
+// Bipolar Console
+/// <summary>
+/// Bipolar Console is an utility class which provides a way to use the unity console in a more encapsulated manner.
+/// These static functions allow for developers to write their own debug messages without them being visible to other
+/// developers. It also avoids the printing of messages if the game is being run in a release version, thus allowing
+/// Debug message calls to stay in the codebase without resulting in performance losses.
+/// Bipolar Console depends upon the BipolarDevID static constants to determine which dev is running the code
+/// and showing the appropriate messages
+/// </summary>
+public class BipolarConsole
+{
+  //--- Core Functions
+  //- private Log(Devs from, string message, Object objectSource = null)
+  //- public ExceptionLog(UnityException exception, Object objectSource = null)
+  #region Core Functions
+
+  /// Log <summary>
+  /// logs a message from a specific developer only if the
+  ///      BipolarDevId is the same developer as "from" or if it has
+  ///      enabled the "see all logs" option. Only visible in editor mode.
+  /// </summary>
+  private static void Log(Devs from, string message, Object objectSource = null) 
+  {
+#if UNITY_EDITOR
+    if (BipolarDevID.iAm == from || BipolarDevID.seeAll)
+    {
+      Debug.Log("[" + from + "] " + message, objectSource);
+    }
+#endif
+	}
+
+  /// ExceptionLog <summary>
+  /// Logs an exception from Unity, usefull for debugging exceptions.Only visible in editor mode.
+  /// </summary>
+  public static void ExceptionLog(UnityException exception, Object objectSource = null)
+  {
+#if UNITY_EDITOR
+    Debug.LogException(exception, objectSource);
+#endif
+  }
+
+  #endregion
+
+
+  //--- Developer specific log
+  //- <DevName>Log(string message, Object objectSource = null)
+  //      Prints message as a specific developer
+  #region Developer Specific Logs
+  /// <summary>
+  /// Write a message to the console as developer Pedro Engana
+  /// </summary>
+  public static void EnganaLog(string message, Object objectSource = null)
+  {
+    Log(Devs.ENGANA, message, objectSource);
+  }
+
+  /// <summary>
+  /// Write a message to the console as developer Hugo Goncalves
+  /// </summary>
+  public static void HugoLog(string message, Object objectSource = null)
+  {
+    Log(Devs.HUGO, message, objectSource);
+  }
+
+  /// <summary>
+  /// Write a message to the console as developer Ivo Capelo
+  /// </summary>
+  public static void IvoLog(string message, Object objectSource = null)
+  {
+    Log(Devs.IVO, message, objectSource);
+  }
+
+  /// <summary>
+  /// Write a message to the console as developer Pedro Lousada
+  /// </summary>
+  public static void LousadaLog(string message, Object objectSource = null)
+  {
+    Log(Devs.LOUSADA, message, objectSource);
+  }
+
+  /// <summary>
+  /// Write a message to the console as developer Rui Dias
+  /// </summary>
+  public static void RuiLog(string message, Object objectSource = null)
+  {
+    Log(Devs.RUI, message, objectSource);
+  }
+
+  /// <summary>
+  /// Write a message to the console which will be visible to all developers
+  /// </summary>
+  public static void AllLog(string message)
+  {
+#if UNITY_EDITOR
+    Debug.Log("[ALL] " + message);
+#endif
+  }
+
+  #endregion
+}
---
+++ BipolarAlpha/Assets/Scripts/Services/DebugLogger/BipolarDevID.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Bipolar Dev ID is a utility class used by Bipolar Console to determine which developer is currently running the software.
+/// Should never be versioned to the repository.
+/// </summary>
+// #ENGANA_TODO Would make more sense that this wasn't a class but an XML file loaded by BipolarConsole. Should be implemented as such down the line.
+public class BipolarDevID
+{
+  // Change these variables for your current run definitions
+  private static Devs _iAm = Devs.ENGANA;
+  private static bool _seeAll = false;
+
+
+  //-- Acessable properties
+  //    iAm - Current developer
+  //    seeAll - boolean that indicates if all debug messages are enabled
+  #region
+  /// <summary>
+  /// Returns the developer currently running the program
+  /// </summary>
+  public static Devs iAm
+  {
+    get
+    {
+      return _iAm;
+    }
+  }
+
+  /// <summary>
+  /// Returns true in case the current developer wants to see all debug messages from all other developers
+  /// </summary>
+  public static bool seeAll
+  {
+    get
+    {
+      return _seeAll;
+    }
+  }
+  #endregion
+}
