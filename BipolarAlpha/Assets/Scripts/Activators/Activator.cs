c6532ac4fd4a346050fd8afb4783155eefb638b0
Duolasa <trynity.5@hotmail.com>
2013-07-25 23:02:57 +0100

    Added the Activator Class

---
+++ BipolarAlpha/Assets/Scripts/Activators/Activator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The Activator class is an abstract class which will be used by game objects that have the need 
+/// to be activated or deactivated. 
+/// </summary>
+public abstract class Activator{
+
+    /// <summary>
+    /// Activates the object. 
+    /// </summary>
+    public abstract void Activate();
+
+    /// <summary>
+    /// Deactivates the object. 
+    /// </summary>
+    public abstract void Deactivate();
+}
