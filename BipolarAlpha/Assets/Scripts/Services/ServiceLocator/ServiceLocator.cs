0908b400e4dfef07b55abce4dea15db97058d98f
RMD291 <RuiMD2.91.@gmail.com>
2013-07-28 17:13:27 +0100

    Added Service Locator Class
    Added the Service Locator class and added a system, stub, class
    (BaseSystem)

---
+++ BipolarAlpha/Assets/Scripts/Services/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Service Locator provides a global point of access to services or systems.
+/// Service Locator permits swapping or changing an implementation of a system with a different one, without having 
+/// to change the classes that use that system, as long as the interface remains the same.
+/// </summary>
+public class ServiceLocator
+{
+  // Variable references to available services/systems
+  private static BaseSystem _baseSys = new BaseSystem();
+
+
+  //--- Access functions
+  //- Get<SystemInterfaceName>()
+  //    Access functions to the available services/systems
+  #region Access Functions
+  /// <summary>
+  /// Returns an implementation of BaseSystem
+  /// </summary>
+  public static BaseSystem GetBaseSystem()
+  {
+      return _baseSys;
+  }
+  #endregion
+
+
+  //--- Provide new system implementation
+  //- Provide<SystemInterfaceName>(<SystemInterfaceName> <systemName>)
+  //    Swaps the current implementation of a system
+  #region Change Service Functions
+  /// <summary>
+  /// Changes the current implementation of BaseSystem to the specified/provided implementation
+  /// </summary>
+  public static void ProvideBaseSystem(BaseSystem baseSys)
+  {
+    _baseSys = baseSys;
+  }
+  #endregion
+}
---
+++ BipolarAlpha/Assets/Scripts/Services/Systems/BaseSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// BaseSystem is a stub class that represents systems offered through the ServiceLocator.
+/// </summary>
+public class BaseSystem
+{
+  //--- Interface/Provided Functions
+  //- doAction()
+  #region Interface/Provided Functions
+  /// <summary>
+  /// Does nothing
+  /// </summary>
+  virtual public void doAction()
+  {
+  }
+  #endregion
+}
