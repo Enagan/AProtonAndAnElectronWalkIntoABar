6b6adf305ad528929667a8071a8d98b5c0b2ec88
RMD291 <ruimd2.91@gmail.com>
2013-09-11 20:37:03 +0100

    Magnet Dispenser
    Added MagnetDispenser Class

---
+++ BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class MagnetDispenser : MonoBehaviour, Activator
+{
+  private bool _isActive = true;
+
+  [SerializeField]
+  private MagneticForce.Charge _charge = MagneticForce.Charge.NEGATIVE;
+
+  #region Activator Interface functions
+  void Activator.Activate()
+  {
+    _isActive = true;
+  }
+
+  void Activator.Deactivate()
+  {
+    _isActive = false;
+  }
+  #endregion
+
+  #region Monobehaviour functions
+  private void Update()
+  {
+    if(_isActive)
+    {
+      if (_charge == MagneticForce.Charge.NEGATIVE)
+      {
+        GameObject magnet = ServiceLocator.GetResourceSystem().InstanceOf("Prefabs/Activators/Magnets/MovableMagnet");
+        magnet.transform.position = this.transform.position + new Vector3(0, 2, 0);
+      }
+      else
+      {
+        GameObject magnet = ServiceLocator.GetResourceSystem().InstanceOf("Prefabs/Activators/Magnets/MovableMagnet");
+        magnet.transform.position = this.transform.position + new Vector3(0, 2, 0);
+      }
+      _isActive = false;
+    }
+  }
+  #endregion
+}
