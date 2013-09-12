6bac9fe6e2aa395da957a617f4d8ac4b794fe970
RMD291 <ruimd2.91@gmail.com>
2013-09-12 12:58:49 +0100

    Added Activatable Light
    Added light activatable by circuits/generator

---
+++ BipolarAlpha/Assets/Scripts/Activators/Light/LightActivator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+class LightActivator : MonoBehaviour, Activator
+{
+  private float _lightIntensity;
+  private Light _associatedLight;
+
+  void Start()
+  {
+    _associatedLight = this.GetComponent<Light>();
+    _lightIntensity = _associatedLight.intensity;
+    Deactivate();
+  }
+
+  public void Activate()
+  {
+    _associatedLight.intensity = _lightIntensity;
+  }
+
+  public void Deactivate()
+  {
+    _associatedLight.intensity = 0;
+  }
+}
