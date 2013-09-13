b63e09b72f16d494995456bbb93ce67ba2850a73
Duolasa <trynity.5@hotmail.com>
2013-09-13 19:26:35 +0100

    Added HexDoors animations

---
+++ BipolarAlpha/Assets/Scripts/Activators/Scenery/HexDoors.cs
@@ -0,0 +1,35 @@
+﻿// Made by: Lousada
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Automatic doors that open when Activated and Close when Deactivated
+/// </summary>
+public class HexDoors : MonoBehaviour, Activator
+{
+  Animation _leftPart;
+  Animation _rightPart;
+  Animation _centerPart;
+
+  public void Start()
+  {
+    _leftPart = (Animation)transform.FindChild("Left").GetComponent(typeof(Animation));
+    _rightPart = (Animation)transform.FindChild("Right").GetComponent(typeof(Animation));
+    _centerPart = (Animation)transform.FindChild("Center").GetComponent(typeof(Animation));
+
+  }
+
+  public void Activate()
+  {
+    _leftPart.Play("TopLeftOpen");
+    _rightPart.Play("TopRightOpen");
+    _centerPart.Play("TopCenterOpen");
+  }
+
+  public void Deactivate()
+  {
+    _leftPart.Play("TopLeftClose");
+    _rightPart.Play("TopRightClose");
+    _centerPart.Play("TopCenterClose");
+  }
+}
