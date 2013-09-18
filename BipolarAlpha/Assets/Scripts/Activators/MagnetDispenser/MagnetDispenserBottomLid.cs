d36078182cfa916ece6d1aa4a6ccf13c7455ee54
Duolasa <trynity.5@hotmail.com>
2013-09-18 14:20:40 +0100

    Added bottom and upper lid animations

---
+++ BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenserBottomLid.cs
@@ -0,0 +1,26 @@
+﻿// Made by: Lousada
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Automatic lids that open when Activated and Close when Deactivated
+/// </summary>
+public class MagnetDispenserBottomLid : MonoBehaviour 
+{
+  Animation _bottomLid;
+
+  public void Start()
+  {
+    _bottomLid = (Animation)transform.FindChild("TopLid").GetComponent(typeof(Animation));
+  }
+
+  public void Activate()
+  { 
+    _bottomLid.CrossFade("DispenserTopOpen");
+  }
+
+  public void Deactivate()
+  {
+    _bottomLid.CrossFade("DispenserTopClose");
+  }
+}
---
+++ BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenserTopLid.cs
@@ -0,0 +1,27 @@
+﻿// Made by: Lousada
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Automatic lids that open when Activated and Close when Deactivated
+/// </summary>
+public class MagnetDispenserTopLid : MonoBehaviour
+{
+  Animation _topLid;
+
+  public void Start()
+  {
+    _topLid = (Animation)transform.FindChild("TopLid").GetComponent(typeof(Animation));
+  }
+
+  public void Activate()
+  { 
+    _topLid.CrossFade("DispenserTopOpen");
+  }
+
+  public void Deactivate()
+  {
+    _topLid.CrossFade("DispenserTopClose");
+  }
+}
+
