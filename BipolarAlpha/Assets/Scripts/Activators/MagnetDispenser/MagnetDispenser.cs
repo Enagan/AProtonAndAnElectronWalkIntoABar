37d3212eb8e166c44356ccabf4b6b119cfd5bd8b
Duolasa <trynity.5@hotmail.com>
2013-09-20 17:25:00 +0100

    Some changes to magnet dispenser animations

--- BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenser.cs
+++ BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenser.cs
@@ -3,38 +3,25 @@ using UnityEngine;
 
 public class MagnetDispenser : MonoBehaviour, Activator
 {
-  private bool _isActive = true;
-
   [SerializeField]
   private MagneticForce.Charge _charge = MagneticForce.Charge.NEGATIVE;
 
   #region Activator Interface functions
   void Activator.Activate()
   {
-    _isActive = true;
+    if (_charge == MagneticForce.Charge.NEGATIVE)
+    {
+      ServiceLocator.GetResourceSystem().InstanceOf("Prefabs/Activators/Magnets/MovableMagnet", this.transform.position);
+    }
+    else
+    {
+      ServiceLocator.GetResourceSystem().InstanceOf("Prefabs/Activators/Magnets/MovableMagnet", this.transform.position);
+    }
   }
 
   void Activator.Deactivate()
   {
-    _isActive = false;
-  }
-  #endregion
-
-  #region Monobehaviour functions
-  private void Update()
-  {
-    if(_isActive)
-    {
-      if (_charge == MagneticForce.Charge.NEGATIVE)
-      {
-        ServiceLocator.GetResourceSystem().InstanceOf("Prefabs/Activators/Magnets/MovableMagnet", this.transform.position + new Vector3(0, 2, 0));
-      }
-      else
-      {
-        ServiceLocator.GetResourceSystem().InstanceOf("Prefabs/Activators/Magnets/MovableMagnet", this.transform.position + new Vector3(0, 2, 0));
-      }
-      _isActive = false;
-    }
+    
   }
   #endregion
 }
--- BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenserTopLid.cs
+++ BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenserTopLid.cs
@@ -5,23 +5,23 @@ using System.Collections;
 /// <summary>
 /// Automatic lids that open when Activated and Close when Deactivated
 /// </summary>
-public class MagnetDispenserTopLid : MonoBehaviour
+public class MagnetDispenserTopLid : MonoBehaviour, Activator
 {
   Animation _topLid;
 
   public void Start()
   {
-    _topLid = (Animation)transform.FindChild("TopLid").GetComponent(typeof(Animation));
+    _topLid = (Animation)transform.GetComponent(typeof(Animation));
   }
 
   public void Activate()
   { 
-    _topLid.CrossFade("DispenserTopOpen");
+    _topLid.CrossFade("TopLidOpen");
   }
 
   public void Deactivate()
   {
-    _topLid.CrossFade("DispenserTopClose");
+    _topLid.CrossFade("TopLidClose");
   }
 }
 
