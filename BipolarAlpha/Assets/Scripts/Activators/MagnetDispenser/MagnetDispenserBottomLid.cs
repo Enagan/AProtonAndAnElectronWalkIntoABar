63cb5f7f89da31273b693322581cda360b1cbe26
Enagan <pedro.a.f.engana@gmail.com>
2013-09-21 14:51:31 +0100

    Le Merge

--- BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenserBottomLid.cs
+++ BipolarAlpha/Assets/Scripts/Activators/MagnetDispenser/MagnetDispenserBottomLid.cs
@@ -5,22 +5,22 @@ using System.Collections;
 /// <summary>
 /// Automatic lids that open when Activated and Close when Deactivated
 /// </summary>
-public class MagnetDispenserBottomLid : MonoBehaviour 
+public class MagnetDispenserBottomLid : MonoBehaviour, Activator
 {
   Animation _bottomLid;
 
   public void Start()
   {
-    _bottomLid = (Animation)transform.FindChild("TopLid").GetComponent(typeof(Animation));
+    _bottomLid = (Animation)transform.GetComponent(typeof(Animation));
   }
 
   public void Activate()
-  { 
-    _bottomLid.CrossFade("DispenserTopOpen");
+  {
+    _bottomLid.CrossFade("BottomLidOpen");
   }
 
   public void Deactivate()
   {
-    _bottomLid.CrossFade("DispenserTopClose");
+    _bottomLid.CrossFade("BottomLidClose");
   }
 }
