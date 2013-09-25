10c39e72a8dc0ecc98983fd168e3655be7739f18
Enagan <pedro.a.f.engana@gmail.com>
2013-09-25 21:21:45 +0100

    Connected all scenes

---
+++ BipolarAlpha/Assets/Scripts/GameplayObjects/CreditsScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScreen : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+  {
+    if (Input.GetKeyUp(KeyCode.Escape))
+    {
+      Application.LoadLevel("MainMenu");
+    }
+	}
+}
---
+++ BipolarAlpha/Assets/Scripts/GameplayObjects/EnderScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnderScript : MonoBehaviour {
+
+	// Use this for initialization
+  void OnTriggerEnter(Collider collider)
+  {
+    if(collider.tag.Equals("Player"))
+    {
+      Application.LoadLevel("CreditsFade");
+    }
+
+  }
+}
