ece2a8d9bb6664263d46f0bd941e5500c27df702
Duolasa <trynity.5@hotmail.com>
2013-09-25 20:55:14 +0100

    Added main menu

---
+++ BipolarAlpha/Assets/Scripts/Services/SceneManager/MainMenu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Return))
+    {
+      GameObject.Find("Main Camera").animation.Play();
+      Invoke("ChangeScene", 1.5f);
+    }
+  }
+
+ void ChangeScene() {
+   Application.LoadLevel("Main");
+     }
+}
+
