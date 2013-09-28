a2616173474b3e351e0c0c1d9d6262fbafed46a4
Enagan <pedro.a.f.engana@gmail.com>
2013-09-28 17:22:08 +0100

    All the Ninja-edits done during VJ2013

--- BipolarAlpha/Assets/Scripts/AnimationEvents/CreditTransition.cs
+++ BipolarAlpha/Assets/Scripts/AnimationEvents/CreditTransition.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class CreditTransition : MonoBehaviour {
 
+    public void Start()
+    {
+      GUITexture logo = GetComponent<GUITexture>();
+
+      logo.pixelInset = new Rect(Screen.width/2-1200/2,Screen.height/2-400/2,1200,400);
+    }
+
 	 public void doTransition( float time )
     {
 		Application.LoadLevel("Credits");
--- BipolarAlpha/Assets/Scripts/GameplayObjects/CreditsScreen.cs
+++ BipolarAlpha/Assets/Scripts/GameplayObjects/CreditsScreen.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class CreditsScreen : MonoBehaviour {
 
+  public void Start()
+  {
+    GUITexture logo = this.transform.FindChild("SM Logo").GetComponent<GUITexture>();
+
+    logo.pixelInset = new Rect((Screen.width/4)/2 -290/2, Screen.height / 2 - 290 / 2 - 55, 290, 290);
+  }
+
 	// Update is called once per frame
 	void Update () 
   {
