08bbd7db29181a41f30804299977a1409b04aa8d
Celcius <ivo.capelo@gmail.com>
2013-12-16 21:02:48 +0000

    AnimationHandlers
    To facilitate access of Animations created two helpers
    AnimationChildHander for objects that have animations
    AnimationRootHandler for objects that contain scripts that call on
    animations

---
+++ BipolarAlpha/Assets/Scripts/Animations/AnimationChildHandler.cs
@@ -0,0 +1,108 @@
+﻿//Made by. Ivo Capelo
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+/// <summary>
+/// AnimationChildHandler is a class that holds animations for an object
+/// The class is meant to be a child in a hiearchy that contains a parent AnimationRootHandler
+/// Scripts that call animations on this object should be located at AnimationRootHandlers and use them to call this object's animations
+/// For usage reffer to instructions in AnimationRootHandler.cs
+/// </summary>
+[RequireComponent(typeof(Animation))]
+public class AnimationChildHandler : MonoBehaviour
+{
+
+  #region private variables
+
+  //_childName is the ID of a child in the hiearchy, every child should have a different name
+  //Different hiearchies can have children with the same name
+  [SerializeField]
+  private string _childName = "";
+
+  //Animation Component of AnimationChildHandler, required
+  private Animation _handlerAnim;
+
+  //Dictionary containing quick access to clips in Animation component and their respective names
+  private Dictionary<string, AnimationState> _dictionaryData;
+
+  #endregion
+
+  #region public properties
+
+  public string childName
+  {
+    get { return _childName; }
+
+    set { _childName = value; }
+  }
+
+  //property of an AnimationChildHandler that refers to available animation clips 
+  public Dictionary<string, AnimationState> animationClips
+  {
+    //Getter of property initializes _dictionaryData at startup
+    get {
+
+      //Get Component if null
+      if (_handlerAnim == null)
+        _handlerAnim = this.GetComponent<Animation>();
+
+      
+      if (_dictionaryData==null && _handlerAnim !=null)
+      {
+        //Create new dictionary
+        _dictionaryData = new Dictionary<string, AnimationState>();
+
+        //Get All animations in Animation component and add links to dictionary
+        foreach (AnimationState clip in _handlerAnim)
+        {
+           _dictionaryData.Add(clip.name, clip);
+        }
+      }
+      return _dictionaryData;
+    }
+
+    //Simple setter
+    set 
+    {
+      _dictionaryData = value; 
+    }
+  }
+
+  #endregion
+
+  #region Monobehavior methods
+  void Start () {
+    //Force naming on AnimationChildHandler
+    if (childName.CompareTo("") == 0)
+    {
+      //TODO Raise Exception
+    }
+    else
+    {
+      if(_handlerAnim == null)
+        _handlerAnim = this.GetComponent<Animation>();
+    }
+	}
+  #endregion
+
+  #region Animation call methods
+
+  //Getter for Animation component
+  public Animation getAnimation()
+  {
+    return _handlerAnim;
+  }
+
+  //Plays clip
+  public void playAnimation(string clipName)
+  {
+    _handlerAnim.Play(clipName);
+  }
+  #endregion
+
+
+}
---
+++ BipolarAlpha/Assets/Scripts/Animations/AnimationRootHandler.cs
@@ -0,0 +1,175 @@
+﻿//Made by. Ivo Capelo
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// #1. Quick and dirty AnimationGuide
+//1. Add AnimationRootHandler to nodes where scripts are meant to call animation
+//2. Add AnimationChildHandler to nodes that should be animated but don't require scripts (ensure there is a RootHandler in a parent to call the child's animation)
+//3. Add Animation Component to all RootHandlesr and ChildHandlers
+//4. Add clips to the Animations array in Animation
+//5. Call animations using methods in #2
+
+//#2 Methods for acessing Animations
+// Calling animation on this Object
+//playAnimation(string clipName);
+
+// Calling animation on Child Object
+//playChildAnimation(string childName, string clipName);
+
+// Getting Animation Components
+// Animation Components can be altered and have additional Play calls such as CrossFade or Blend
+//getAnimation() - For this object
+//getChildAnimation(string childName) - For child
+
+/// <summary>
+/// AnimationRootHandler is an AnimationChildHandler tasked with aggregating animations in object hiearchy to facilitate calling animations
+/// The class is meant to be a parent node and should be called from scripts for object animation
+/// Requires the Animation component in the object
+/// </summary>
+[RequireComponent(typeof(Animation))]
+public class AnimationRootHandler : AnimationChildHandler
+{
+
+  #region private variables
+
+  //Symbol used to split keys between child name and clip name
+  private static char _splitSymbol = ':';
+
+  //Dictionary for quick acess to children with certain animations
+  //Keys can be obtained with getAnimChildrenKey(string clipName, string childName) or getAnimChildrenKey(string clipName, AnimationChildHandler child)
+  private Dictionary<string, AnimationChildHandler> _animChildren = new Dictionary<string,AnimationChildHandler>();
+
+  #endregion
+
+  #region Monobehavior methods
+
+  // Use this for initialization
+	void Start () {
+    //Initialize _animChildren with all animations in hiearchy below
+    initializeAnimations();
+	}
+
+  #endregion
+
+  #region Startup register AnimationChildHandler methods
+
+  private void initializeAnimations()
+  {
+    //Get self, all children, and children of children, with animation handlers
+    AnimationChildHandler[] array = this.GetComponentsInChildren<AnimationChildHandler>();
+    foreach(AnimationChildHandler child in array)
+    {
+     // Register Children and Self
+     registerAnimationsInChildHandler(child);
+    }
+  }
+
+  // Registers all animations from an AnimationChildHAndler in _animChildren
+  private void registerAnimationsInChildHandler(AnimationChildHandler handler)
+  {
+    if(handler.animationClips!=null && handler.animationClips.Count> 0)
+      foreach (string animName in handler.animationClips.Keys)
+      {
+        //Register pairing of animation and handler for easy acquisition and call of animation
+        string pairingID = getAnimChildrenKey(animName,handler);
+        _animChildren.Add(pairingID,handler);
+      }
+  }
+
+  #endregion
+
+  #region Key Construction static methods
+
+  // Returns key for acessing _animChildren
+  public static string getAnimChildrenKey(string anim, AnimationChildHandler child)
+  {
+    // Key is formed from "ChildName _splitSymbol anim"
+    // Child "LeftMagnet"'s animation "Turn" will have the key "LeftMagnet:Turn", assuming ':' as splitSymbol
+    return child.childName+_splitSymbol+anim;
+  }
+
+  // Returns key for acessing _animChildren
+  public static string getAnimChildrenKey(string anim, string childName)
+  {
+    // Key is formed from "ChildName _splitSymbol anim"
+    // Child "LeftMagnet"'s animation "Turn" will have the key "LeftMagnet:Turn", assuming ':' as splitSymbol
+    return childName+_splitSymbol+anim;
+  }
+
+  // Returns the clipName from a key
+  public static string getAnimNameFromAnimChildrenKey(string key)
+  {
+    string[] split = key.Split(new char[] {_splitSymbol});
+    if(split.Length !=2)
+    {
+      //TODO RAISE EXCEPTION
+      return "NOPE";
+    }
+    else
+    {
+      //returns animation name
+      return split[1];
+    }
+  }
+
+  // Returns the childName from a key
+  public static string getChildNameFromAnimChildrenKey(string key)
+  {
+    string[] split = key.Split(new char[] {_splitSymbol});
+    if(split.Length !=2)
+    {
+      //TODO RAISE EXCEPTION
+      return "NOPE";
+    }
+    else
+    {
+      //returns animation name
+      return split[0];
+    }
+  }
+
+  #endregion
+
+  #region Child animation accessing methods
+
+  // Plays clip in child
+  public void playChildAnimation(string childName,string clipName)
+  {
+    string key = getAnimChildrenKey(clipName, childName);
+    AnimationChildHandler child = _animChildren[key];
+    child.playAnimation(clipName);
+  }
+
+  // Returns Animation component in child
+  public Animation getChildAnimation(string childName)
+  {
+
+    foreach (string key in _animChildren.Keys)
+    {
+      if(getChildNameFromAnimChildrenKey(key).CompareTo(childName) == 0)
+      {
+        return _animChildren[key].getAnimation();
+      }
+    }
+    //TODO no such child exception
+    return null;
+  }
+
+  #endregion
+
+  #region Debug methods
+
+  //Dumps into console all animations in RootHandler and respective children
+  private void dumpAnimations()
+  {
+    BipolarConsole.AllLog("<Dumping Animations in ROOT:" + childName);
+    foreach (string key in _animChildren.Keys)
+    {
+      BipolarConsole.AllLog(key);
+    }
+    BipolarConsole.AllLog("Dump end>");
+  }
+
+  #endregion
+}
