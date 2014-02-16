f374e9405dfce0304f2a432a3b017fed1b88c104
RMD291 <ruimd2.91@gmail.com>
2014-02-16 15:34:28 +0000

    Railgun - Added Railgun Model, Code and Railgun Cutscene

---
+++ BipolarAlpha/Assets/Scripts/Activators/Railgun/Railgun.cs
@@ -0,0 +1,46 @@
+﻿//Made by: Rui
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+/// <summary>
+/// </summary>
+public class Railgun : MonoBehaviour, Activator
+{
+  #region Railgun Constants
+  private const float FORCEMULTIPLIER = 50.0f;
+  #endregion
+
+  #region Railgun Variables
+  [SerializeField]
+  private bool _isActivated = false;
+  #endregion
+
+  #region Railgun Properties
+
+  public bool isActivated
+  {
+    get { return _isActivated; }
+  }
+  #endregion
+
+  void Activator.Activate()
+  {
+    _isActivated = true;
+  }
+
+  void Activator.Deactivate()
+  {
+    _isActivated = false;
+  }
+
+  public virtual void Start() { }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(_isActivated && other.tag == "Player")
+    {
+      other.rigidbody.AddForce( -1 * this.transform.right * FORCEMULTIPLIER, ForceMode.Force);
+      ForceComponent.applyForce(other.gameObject, -1 * this.transform.right * FORCEMULTIPLIER, 5.0f);
+    }
+  }
+}
---
+++ BipolarAlpha/Assets/Scripts/Misc. Gameplay/ForceComponent/ForceComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+
+class ForceComponent : MonoBehaviour
+{
+  #region Variables
+  private Vector3 _force = Vector3.zero;
+  private float _time = 0.0f;
+  #endregion
+
+  private void Start()
+  {
+    if(this.rigidbody == null)
+    {
+      Debug.Log("ForceComponent not associated with a rigid body.");
+    }
+  }
+
+  private void Update()
+  {
+    if(_time <= 0.0f)
+    {
+      Destroy(this);
+    }
+    this.rigidbody.AddForce(_force*_time, ForceMode.Impulse);
+    _time -= Time.deltaTime;
+  }
+
+  public void applyForce(Vector3 force, float duration)
+  {
+    _time = duration;
+    _force = force;
+  }
+
+  public static void applyForce(GameObject target, Vector3 force, float duration) 
+  {
+    ForceComponent fc = target.AddComponent<ForceComponent>();
+    fc.applyForce(force, duration);
+  }
+}
