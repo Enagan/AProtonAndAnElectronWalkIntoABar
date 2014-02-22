d685fc5032be74da898805f266583366ed66a28c
Duolasa <trynity.5@hotmail.com>
2014-02-22 21:53:24 +0000

    Forgot some stuff on the last commit

---
+++ BipolarAlpha/Assets/Scripts/Services/HUDSystem/HUDObjects/PlayerPanel/HUDBootSeq.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDBootSeq : HUDObject{
+
+    #region private variables
+    // Text style elements
+    private GUIStyle BIOSStyle;
+    Texture2D textureBlack;
+
+    // Current and stored tooltips
+    private string displayText = "";
+    private string fullText = "MAGBOT9000 Control System v4.85.6.1 [Feb 24 2087] \n\n\n\nBooting...\n\n\n\nMAGBOT9000 was not shut down properly. Initiating full systems check.\n\n\n\nHUD System...[OK]\n\nHand Magnet System...[OK]\n\nArm Control System...[OK]\n\nLocomotor System...[OK]\n\n\n\nAll Systems OK\n\n\n\nActivating HUD";
+    private Rect texturePosition;
+
+    private bool onBootSeq;
+    private float delay = 20.0f;
+    private float characterDelay = 0.015f;
+
+    private float nextUsage;
+    private float nextCharacterTimer;
+    private Rect textArea;
+
+    #endregion
+
+	  #region HUD methods
+	
+  // Constructor
+    public HUDBootSeq(int priority) : base(priority)
+  {
+      textureBlack = new Texture2D(1, 1);
+      textureBlack.SetPixel(0, 0, Color.black);
+      textureBlack.Apply();
+
+      texturePosition = new Rect(0, 0, Screen.width , Screen.height);
+
+      textArea = new Rect(Screen.width / 80, Screen.height / 50, Screen.width - Screen.width / 20, Screen.height - Screen.height / 20);
+
+      onBootSeq = true;
+      nextUsage = Time.time + delay;
+      nextCharacterTimer = Time.time + characterDelay;
+
+      // Set text style
+
+      GUISkin mySkin = (GUISkin)Resources.Load("GUI/Skins/BootSequence");
+      BIOSStyle = mySkin.label;
+
+  }
+	
+  public override void DrawHUD()
+  {
+      if (onBootSeq)
+      {
+          if (Time.time > nextUsage)
+          {
+              onBootSeq = false;
+              return;
+          }
+
+          TextAnimation();
+
+          GUI.DrawTexture(texturePosition, textureBlack);
+
+          GUI.Label(textArea, displayText, BIOSStyle);
+
+      }
+
+  }
+	
+  #endregion
+
+  #region Auxiliary Functions
+
+  void TextAnimation()
+  {
+      if (fullText.Length > 0)
+      {
+
+          if (Time.time > nextCharacterTimer)
+          {
+              if (fullText.StartsWith("\n\n\n\n") || fullText.StartsWith(".["))
+              {
+                  nextCharacterTimer = Time.time + characterDelay * 100;
+
+              }
+              else
+              {
+                  nextCharacterTimer = Time.time + characterDelay;
+              }
+              displayText += fullText.Substring(0,1);
+              fullText = fullText.Remove(0,1);
+          }
+      }
+  }
+    
+  #endregion
+
+}
