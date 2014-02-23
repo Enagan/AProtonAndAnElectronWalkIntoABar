c3a8c29a272e9c40cd1bca39c8bf9e876fe62488
Duolasa <trynity.5@hotmail.com>
2014-02-23 21:34:39 +0000

    Added trigger animation to hoop

---
+++ BipolarAlpha/Assets/Scripts/Activators/Scenery/HoopAnimationTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoopAnimationTrigger : MonoBehaviour {
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Magnet")
+        {
+            this.transform.parent.GetComponentInChildren<Animation>().Play();
+        }
+    }
+}
