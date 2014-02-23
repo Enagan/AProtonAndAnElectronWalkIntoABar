e87551b94298ca0e4d0f5d64164b24716d6e4732
Celcius <ivo.capelo@gmail.com>
2014-02-23 16:12:43 +0000

    QaTest1 done

---
+++ BipolarAlpha/Assets/Scripts/Activators/Scenery/PlatformManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformManager : MonoBehaviour {
+
+  public void activatePlatforms()
+  {
+    AnimationRootHandler anim = GetComponent<AnimationRootHandler>();
+    if (anim)
+    {
+      anim.playChildAnimation("Platform2", "WeightPlatformStart");
+      anim.playChildAnimation("Platform3", "WeightPlatformStart");
+      anim.playChildAnimation("Platform4", "WeightPlatformStart");
+    }
+  }
+}
