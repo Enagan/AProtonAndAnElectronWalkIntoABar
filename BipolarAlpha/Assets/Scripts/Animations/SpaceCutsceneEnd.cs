8d92d7747bf41ea5fd8123182fd13961838f65c9
Duolasa <trynity.5@hotmail.com>
2014-02-24 03:59:33 +0000

    Forgot some crap

---
+++ BipolarAlpha/Assets/Scripts/Animations/SpaceCutsceneEnd.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceCutsceneEnd : MonoBehaviour {
+
+	// Use this for initialization
+    void goToMain()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
