a0383a03557f6e1bb6d62ef126ca29e30181665c
Celcius <ivo.capelo@gmail.com>
2014-02-24 04:09:51 +0000

    Doors

---
+++ BipolarAlpha/Assets/Scripts/Activators/Scenery/BarredDoorSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarredDoorSound : MonoBehaviour {
+
+  void playLock()
+  {
+    ServiceLocator.GetAudioSystem().PlayQuickSFX("lock open",transform.position,0.5f);
+  }
+
+
+  void playDoorSound()
+  {
+    BipolarConsole.AllLog("printing");
+    ServiceLocator.GetAudioSystem().PlayQuickSFX("Door",transform.position,1);
+  }
+
+
+}
---
+++ BipolarAlpha/Assets/Scripts/Activators/Scenery/DoorOpenSounds.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenSounds : MonoBehaviour {
+
+  void lockOpen()
+  {
+    ServiceLocator.GetAudioSystem().PlayQuickSFX("lock open",transform.position,0.5f);
+  }
+
+  void doorOpen()
+  {
+    ServiceLocator.GetAudioSystem().PlayQuickSFX("Door",transform.position,1);
+  }
+}
