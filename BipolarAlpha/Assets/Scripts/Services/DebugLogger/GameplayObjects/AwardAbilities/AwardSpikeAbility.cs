357b8ce91fbd4fa4dc5dd27f32c3820bf960b6da
Celcius <ivo.capelo@gmail.com>
2014-07-19 18:11:53 +0100

    Pickable Spike powerup

---
+++ BipolarAlpha/Assets/Scripts/Services/DebugLogger/GameplayObjects/AwardAbilities/AwardSpikeAbility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Awards the player with spike ability when collided with
+///  Requires a Collision Volume with onTrigger enabled
+/// </summary>
+public class AwardSpikeAbility : AwardPlayerAbility 
+{
+
+  /// <summary>
+  ///  Called by parent when player collides, awards player the SpikeAbility
+  /// </summary>
+  public override void awardAbility(PlayerController player)
+  {
+    BipolarConsole.AllLog("Awarding Spike Ability");
+    player.addStickyMagnetAbilities();
+
+  }
+}
