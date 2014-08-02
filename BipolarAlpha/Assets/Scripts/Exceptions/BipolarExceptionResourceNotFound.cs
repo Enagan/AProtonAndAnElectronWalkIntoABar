36d3dc006bb3e971b37dcb6b252a5880eebf9e04
Enagan <pedro.a.f.engana@gmail.com>
2014-08-02 22:44:15 +0100

    New Exception

---
+++ BipolarAlpha/Assets/Scripts/Exceptions/BipolarExceptionResourceNotFound.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BipolarExceptionResourceNotFound : BipolarException 
+{
+  public BipolarExceptionResourceNotFound(string message) : base(message) { }
+}
+
