bcea964d4055c47efb8ba6502c517b168cf3a61e
RMD <ruimd2.91@gmail.com>
2014-09-01 10:57:57 +0100

    Added controllable Elevator script, prefab and to the scene. Also added a console near the elevator to activate it.

---
+++ BipolarAlpha/Assets/Scripts/Jacked-In/JackedInElevatorControler.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JackedInElevatorControler : JackedInRemoteController {
+
+  public enum ElevatorMode {STOP, MOVE, AT_BOTTOM, AT_TOP};
+
+  [SerializeField]
+  private float _elevatorSpeed = 5.0f;
+
+  private ElevatorMode _activeMode = ElevatorMode.STOP;
+
+  public override void Left()
+  {
+    if (_activeMode != ElevatorMode.AT_TOP)
+    {
+      _activeMode = ElevatorMode.MOVE;
+      if (_elevatorSpeed < 0.0f)
+      {
+        _elevatorSpeed *= -1.0f;
+      }
+    }
+  }
+
+  public override void Right()
+  {
+    if (_activeMode != ElevatorMode.AT_BOTTOM)
+    {
+      _activeMode = ElevatorMode.MOVE;
+      if (_elevatorSpeed > 0.0f)
+      {
+        _elevatorSpeed *= -1.0f;
+      }
+    }
+  }
+
+  private void Update()
+  {
+    if (_activeMode == ElevatorMode.MOVE)
+    {
+      foreach (GameObject go in _activatableObjects)
+      {
+        Move(go);
+      }
+    }
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.name == "ElevatorStopFloor")
+    {
+      _activeMode = ElevatorMode.STOP;
+    }
+    else if (other.gameObject.name == "ElevatorTop")
+    {
+      _activeMode = ElevatorMode.AT_TOP;
+    }
+    else if (other.gameObject.name == "ElevatorBottom")
+    {
+      _activeMode = ElevatorMode.AT_BOTTOM;
+    }
+  }
+
+  private void Move(GameObject go)
+  {
+    go.transform.position += go.transform.up * _elevatorSpeed * Time.deltaTime;
+  }
+}
