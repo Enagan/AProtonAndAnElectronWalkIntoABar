b55f4fea90cb248501101fd81d94a8b4c7718b9a
RMD <ruimd2.91@gmail.com>
2014-09-01 20:35:46 +0100

    "Fix" to the elevator ("WIP")

--- BipolarAlpha/Assets/Scripts/Jacked-In/JackedInElevatorControler.cs
+++ BipolarAlpha/Assets/Scripts/Jacked-In/JackedInElevatorControler.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class JackedInElevatorControler : JackedInRemoteController {
 
@@ -9,6 +10,13 @@ public class JackedInElevatorControler : JackedInRemoteController {
   private float _elevatorSpeed = 5.0f;
 
   private ElevatorMode _activeMode = ElevatorMode.STOP;
+
+  [SerializeField]
+  private GameObject _elevatorBottom;
+  [SerializeField]
+  private GameObject _elevatorTop;
+  [SerializeField]
+  private List<GameObject> _elevatorFloors = new List<GameObject>();
 
   public override void Left()
   {
@@ -40,29 +48,38 @@ public class JackedInElevatorControler : JackedInRemoteController {
     {
       foreach (GameObject go in _activatableObjects)
       {
+        Check();
         Move(go);
       }
-    }
-  }
-
-  private void OnTriggerEnter(Collider other)
-  {
-    if (other.gameObject.name == "ElevatorStopFloor")
-    {
-      _activeMode = ElevatorMode.STOP;
-    }
-    else if (other.gameObject.name == "ElevatorTop")
-    {
-      _activeMode = ElevatorMode.AT_TOP;
-    }
-    else if (other.gameObject.name == "ElevatorBottom")
-    {
-      _activeMode = ElevatorMode.AT_BOTTOM;
     }
   }
 
   private void Move(GameObject go)
   {
     go.transform.position += go.transform.up * _elevatorSpeed * Time.deltaTime;
+  }
+
+  private void Check()
+  {
+    if (this.transform.position.y <= _elevatorBottom.transform.position.y)
+    {
+      _activeMode = ElevatorMode.AT_BOTTOM;
+      this.transform.position = new Vector3(this.transform.position.x, _elevatorBottom.transform.position.y, this.transform.position.z);
+    }
+    else if (this.transform.position.y >= _elevatorTop.transform.position.y)
+    {
+      _activeMode = ElevatorMode.AT_TOP;
+      this.transform.position = new Vector3(this.transform.position.x, _elevatorTop.transform.position.y, this.transform.position.z);
+    }
+    else
+    {
+      foreach (GameObject floor in _elevatorFloors)
+      {
+        if (this.transform.position.y == floor.transform.position.y)
+        {
+          _activeMode = ElevatorMode.STOP;
+        }
+      }
+    }
   }
 }
